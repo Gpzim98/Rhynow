b1a917cdc457ca1ada6f9e289000fd718729f72f
Gregory <gpzim98@gmail.com>
2021-08-07 15:50:37 +0100

    Playing with the IoC container and different dependency injections

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using ProductsCatalog.Application;
 using ProductsCatalog.Application.Interfaces;
+using Infrastructure.IoC;
 
 namespace ConsoleApplication
 {
+    class IoCContainer : IoCSupportedTest<ConsoleApplicationIoCContainer> 
+    {
+        public ICatalog productsCatalog;
+        public IoCContainer()
+        { 
+            productsCatalog = Resolve<ICatalog>();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            ICatalog catalog = new Catalog();
-            catalog.GetAllCategories();
-            Console.WriteLine("Hello World!");
+            IoCContainer ioCContainer = new IoCContainer();
+            ICatalog catalog = ioCContainer.productsCatalog;
+            var category = catalog.GetCategoryById(Guid.NewGuid());
+            Console.WriteLine(category.Name);
         }
     }
 }
